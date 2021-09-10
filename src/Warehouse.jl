799d3ac38d929b404076583977bc478d96bc8274
Vojtech Rybar <rybarvoj@fit.cvut.cz>
2021-09-10 15:55:56 +0200

    in the experiments

--- src/Warehouse.jl
+++ src/Warehouse.jl
@@ -9,5 +9,6 @@ export get_experiment_name,init_warehouse_with_plot,terminate_warehouse_sim, ani
 export adata,mdata,seeds
 export ed_hw3x3_10_2,ed_hw17x15_1000_35, ed_cbs17x15_1000_35,ed_cbsd17x15_1000_35,ed_cbs3x3_10_2
 export ed_cbs17x15_10_5
+export ed_cbs17x15_1000_20,ed_hw17x15_1000_20,ed_cbsd17x15_1000_20
 
 end 
--- src/definitions.jl
+++ src/definitions.jl
@@ -44,3 +44,35 @@ end
 load_spots_17x15 = collect(32:4:60);
 wh17x15_10_5_undirected = WarehouseDefinition(17,15,10,5,SimpleGraph,init_warehouse,init_robots_17x15_5,load_spots_17x15)
 ed_cbs17x15_10_5 = ExperimentDefinition("cbs17x15_10_5",wh17x15_10_5_undirected, robot_step_cbs!,warehouse_step_cbs!)
+
+# 17x15 20 1000 
+function init_robots_17x15_20(warehouse)
+    for (i,j) in enumerate(31:31:620)
+        add_agent!(Robot(i,j,0,1,[],0,0),j, warehouse)
+        # n_agents += 1
+    end
+end
+load_spots_17x15 = collect(32:4:60);
+wh17x15_1000_20_undirected = WarehouseDefinition(17,15,1000,20,SimpleGraph,init_warehouse,init_robots_17x15_20,load_spots_17x15)
+wh17x15_1000_20_directed = WarehouseDefinition(17,15,1000,20,SimpleDiGraph,init_warehouse,init_robots_17x15_20,load_spots_17x15)
+
+ed_hw17x15_1000_20 = ExperimentDefinition(  "hw17x15_1000_20",wh17x15_1000_20_directed,robot_step_highway_nowait!,warehouse_step_highway!)
+ed_cbs17x15_1000_20 = ExperimentDefinition( "cbs17x15_1000_20",wh17x15_1000_20_undirected, robot_step_cbs!,warehouse_step_cbs!)
+ed_cbsd17x15_1000_20 = ExperimentDefinition("cbsd17x15_1000_20",wh17x15_1000_20_directed, robot_step_cbs!,warehouse_step_cbs!)
+
+
+# 119x15
+load_spots_119x5 = collect(12:4:20)
+function init_robots_119x5_20(warehouse)
+    for (i,j) in enumerate(11:11:220)
+        add_agent!(Robot(i,j,0,1,[],0,0),j, warehouse)
+        # n_agents += 1
+    end
+end
+wh19x5_directed = WarehouseDefinition(19,5,100,20,SimpleDiGraph,init_warehouse,init_robots_119x5_20,load_spots_119x5)
+wh19x5_undirected = WarehouseDefinition(19,5,100,20,SimpleGraph,init_warehouse,init_robots_119x5_20,load_spots_119x5)
+                    
+ed_hw119x5_100_20 = ExperimentDefinition("hw119x5_100_20",wh19x5_directed,robot_step_highway_nowait!,warehouse_step_highway!)
+ed_cbsd119x5_100_20 = ExperimentDefinition("cbsd119x5_100_20",wh19x5_directed, robot_step_cbs!,warehouse_step_cbs!)
+ed_cbs119x5_100_20 = ExperimentDefinition("cbs119x5_100_20",wh19x5_undirected, robot_step_cbs!,warehouse_step_cbs!)
+
