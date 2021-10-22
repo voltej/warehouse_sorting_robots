99c8b3c76277c3507b4073e7bf8803c73d3ab4e9
Vojtech Rybar <rybarvoj@fit.cvut.cz>
2021-10-22 10:13:46 +0200

    cleenup

--- src/Warehouse.jl
+++ src/Warehouse.jl
@@ -6,7 +6,7 @@ include("cbs.jl")
 include("definitions.jl")
 
 export get_experiment_name,init_warehouse_with_plot,terminate_warehouse_sim, animate_warehouse
-export adata,mdata,seeds
+export adata,mdata
 
 export ed_hw3x3_10_2,ed_hw17x15_1000_35, ed_cbs17x15_1000_35,ed_cbsd17x15_1000_35,ed_cbs3x3_10_2
 export ed_cbs17x15_10_5
